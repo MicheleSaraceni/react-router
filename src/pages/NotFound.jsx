dde00ebb116c076c9e91e890b36f103a6152b5a0
MicheleSaraceni <michele.saraceni23@email.com>
2025-01-10 17:14:56 +0100

    adaptation fixed and added dedicate page for AddPost

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="d-flex justify-content-center align-items-center">
+            <h1>NotFound</h1>
+        </div>
+    )
+}
